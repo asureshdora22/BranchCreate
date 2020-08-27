168fe77ef144b560b5d88601c1fb6f772e74c964
suresh <asureshdora8@gmail.com>
2020-08-27 23:47:45 +0530

    add A.java

--- src/main/java/com/suresh/SpringBoot3Application.java
+++ src/main/java/com/suresh/SpringBoot3Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBoot3Application {
-
+int k=40;
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBoot3Application.class, args);
 	}
